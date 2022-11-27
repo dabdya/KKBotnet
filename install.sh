8325db6275a9632ce13c486b5dd6a03be5a8100a
Knyazzini <knyazzev.lgor@gmail.com>
2022-11-27 18:47:04 +0500

    add daemon config

---
+++ install.sh
@@ -0,0 +1,19 @@
+sudo touch /etc/systemd/system/botnet.service;
+cfg="[Unit]
+Description=botnet
+After=network.target
+
+[Service]
+User='$(whoami)'
+Group='$(id -g)'
+WorkingDirectory='$(pwd)'
+ExecStart=/usr/bin/python3 $(pwd)/src/main.py
+TimeoutSec=30
+Restart=always
+
+[Install]
+WantedBy=multi-user.target"
+sudo bash -c "echo '$cfg' > /etc/systemd/system/botnet.service";
+echo "$cfg"
+sudo systemctl enable botnet
+sudo systemctl start botnet
